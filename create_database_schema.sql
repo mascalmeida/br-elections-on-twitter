8be030ec6bf08413c017f1c47acfcbd3c987a050
mascalmeida <lucasmascalmeida@gmail.com>
2022-09-18 19:29:14 -0300

    feat[ETL]: SQL script creation and old python codes deletion

---
+++ create_database_schema.sql
@@ -0,0 +1,34 @@
+/* 
+Twitter & BR-Elections project
+Tool: MySQL
+Authors: @mascalmeida & @jorgel-mendes
+*/
+
+-- CREATE DATABASE
+CREATE DATABASE IF NOT EXISTS br_elections;
+USE br_elections;
+
+-- CREATE PROFILE MENTIONS TABLE
+DROP TABLE IF EXISTS profile_mentions;
+CREATE TABLE IF NOT EXISTS profile_mentions (
+	`start` DATE NOT NULL,
+	start_date DATE NOT NULL,
+	start_time DATE NOT NULL,
+	`end` DATE NOT NULL,
+	end_date DATE NOT NULL,
+	end_time DATE NOT NULL,
+    `@LulaOficial_mentions` INT,
+    `@LulaOficial_mentions_without_retweet` INT
+);
+
+-- CREATE PROFILE INFO TABLE
+DROP TABLE IF EXISTS profile_info;
+CREATE TABLE IF NOT EXISTS profile_info (
+	`date` DATE NOT NULL,
+    screen_name VARCHAR(32) NOT NULL,
+    followers INT,
+    `following` INT,
+    posts INT,
+    lists INT,
+    likes INT
+);
